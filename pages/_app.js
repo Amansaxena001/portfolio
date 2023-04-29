efd227793a85c8957925d26632eee7768fb78642
aman.saxena <saxenaaman800@gmail.com>
2023-04-29 20:42:26 +0530

    ui-animations

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useEffect } from 'react';
+import Aos from 'aos';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        Aos.init();
+    }, []);
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
